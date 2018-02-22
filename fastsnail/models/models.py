f173a1ae4d0dac0776e350a11f1dcada007e3c66
Mancheguet <mancheguet96@gmail.com>
2018-02-22 19:43:16 +0100

    Copia_Funcional_Final_Entrega

--- fastsnail/models/models.py
+++ fastsnail/models/models.py
@@ -12,30 +12,27 @@ class varietysnail(models.Model):
     idvariety = fields.Integer()
     thermalresist = fields.Char()
     description = fields.Text()
-    totalsnails = fields.One2many('fastsnail.snail','snailclass')#fields.Integer() #total de caracoles que tienen este timpo , tendrá que ser computada  y se dará en kg
+    totalsnails = fields.One2many('res.partner','snailclass')#fields.Integer() #total de caracoles que tienen este timpo , tendrá que ser computada  y se dará en kg
 
 
 class snail(models.Model):
-    _name = 'fastsnail.snail'
+    _name = 'res.partner'
+    _inherit ='res.partner'
 
     name = fields.Char()
     idsnail = fields.Char()
     snailclass = fields.Many2one('fastsnail.varietysnail')#fields.Char() #tendrá que darte a elegir entre las classe creada anteriomente (la parte de arriba)
-    totalraces = fields.Many2many('fastsnail.race', 'totalsnails')
+    totalraces = fields.Many2many('fastsnail.race')
     description = fields.Text()
     photo = fields.Binary() # foto del caracol
+    is_snail = fields.Boolean()
 
-    # @api.constrains('idsnail')
-    # def _check_value(self):
-    #      for r in self.search([]):
-    #         for x in self.search([]):
-    #             if r.id != x.id: 
-    #                 if r.idsnail == x.idsnail:
-    #                     raise ValidationError("El ids de caracol ya esxiste.")
-
-            #snailsexistentes = self.search_count([('idsnail', '=', r.idsnail)])
-            # if len(snailsexistentes) > 0:
-            #     raise ValidationError("El ID de caracol ya esxiste.")
+    @api.constrains('idsnail')
+    def _check_value(self):
+    	for r in self:
+    		caracolesId = self.search([('idsnail', '=', r.idsnail)])
+    		if len(caracolesId) > 1:
+    			raise ValidationError("El Id del caracol ya existe.")
 
 class competition(models.Model):
     _name = 'fastsnail.competition'
@@ -43,6 +40,11 @@ class competition(models.Model):
     name = fields.Char()
     idcompetition = fields.Integer()
     totalSeasons = fields.One2many('fastsnail.season','competition')
+
+    @api.multi
+    def copy_competition(self):
+        self.env['fastsnail.competition'].create({'name':self.name+"_copia", 'idcompetition':self.idcompetition+1, 'totalSeasons':self.totalSeasons})
+
 
 class season(models.Model):
     _name = 'fastsnail.season'
@@ -55,33 +57,72 @@ class season(models.Model):
     competition = fields.Many2one('fastsnail.competition')
     totalrace = fields.One2many('fastsnail.race','season') #total de carreras con lista , computada de las carreras que esten en esta season
 
+    @api.constrains('idseason')
+    def _check_value(self):
+    	for r in self:
+    		seasonsId = self.search([('idseason', '=', r.idseason)])
+    		if len(seasonsId) > 1:
+    			raise ValidationError("El Id de la Season ya existe.")
+
 class race(models.Model):
     _name = 'fastsnail.race'
 
     name = fields.Char()
     idrace = fields.Integer()
+    season = fields.Many2one('fastsnail.season')
     startdate = fields.Datetime() #data en la que empieza la temporada
     description = fields.Text()
-    season = fields.Many2one('fastsnail.season')
-    totalsnails = fields.Many2many('fastsnail.snail','totalraces') #total de caracoles participan en la temporada computado usando la lista de las carreras
+    totalsnails = fields.Many2many('res.partner') #total de caracoles participan en la temporada computado usando la lista de las carreras
+    
+    @api.constrains('idrace')
+    def _check_value(self):
+    	for r in self:
+    		idraceId = self.search([('idrace', '=', r.idrace)])
+    		if len(idraceId) > 1:
+    			raise ValidationError("El Id de la Carrera ya existe.")
+
+    @api.constrains('startdate')
+    def _chek_value(self):
+        for r in self:
+            if r.startdate>r.season.enddate:
+                raise ValidationError("La fecha tiene que estar dentro de la Season")
+    
 
 class classification(models.Model):
 	_name = 'fastsnail.classification'
 
 	name = fields.Char()
-	idclassification = fields.Integer()
-	idrace = fields.Integer() # relación entre la carrera y la classificación ( tendrá que dar a elegir que carrera queremos)
-	idfirstsnail = fields.Many2one('fastsnail.snail')
-	idsecondsnail = fields.Many2one('fastsnail.snail')
-	idthirdsnail = fields.Many2one('fastsnail.snail')
+	idclassification = fields.Char()
+	idrace = fields.Many2one('fastsnail.race') # relación entre la carrera y la classificación ( tendrá que dar a elegir que carrera queremos)
+	idfirstsnail = fields.Many2one('res.partner')
+	idsecondsnail = fields.Many2one('res.partner')
+	idthirdsnail = fields.Many2one('res.partner')
 
-
+	@api.constrains('idclassification', 'idrace')
+	def _check_value(self):
+		for x in self:
+			idclassificationmomento = self.search([('idclassification', '=', x.idclassification)])
+			idraceId = self.search([('idrace', '=', x.idrace.idrace)])
+			if len(idclassificationmomento) > 1:
+				raise ValidationError("Este id de classificacion ya existe")
+			if len(idraceId) > 1:
+				raise ValidationError("Esta carrera ya tiene registrada una puntuacion.")
+			
 class employee(models.Model):
 	_name = 'fastsnail.employee'
 
-  	idemployee = fields.Integer()
+  	idemployee = fields.Char()
 	name = fields.Char()
 	surname = fields.Char()
 	phone = fields.Integer()
 
+	@api.constrains('idemployee')
+	def _check_value(self):
+		for r in self:
+			idemployeess = self.search([('idemployee', '=', r.idemployee)])
+			if len(idemployeess) > 1:
+				raise ValidationError("El Id de empleado ya existe")
+
 #CREAR UNA TAULA INTERMIG -> CARERRA-CARAGOL-POSICIÓ
+
+
